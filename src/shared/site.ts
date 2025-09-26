fc14c2ddd841da5161a69c5c95f301edb0fdb13f
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-09-25 22:41:26 -0500

    feat: agregar site general

---
+++ src/shared/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "Semillero Digital Dashboard",
+  description:
+    "El siguiente sitio web busca resolver la organización de Google Classroom para que la herramiente sea más accesible.",
+  navItems: [
+    {
+      label: "Nosotros",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Nosotros",
+      href: "/about",
+    },
+  ],
+};
